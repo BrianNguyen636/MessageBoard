f5da344b939523262af8d43740c9f25bd8869bd4
Brian Nguyen <briannguyen636@gmail.com>
2025-03-02 10:57:49 -0800

    beginning node backend

--- insert_fields.sql
+++ insert_fields.sql
@@ -29,17 +29,17 @@ INSERT INTO Threads (title) VALUES
 	('Kelvin bros')
 
 INSERT INTO Posts(threadID, userID, replyID, body, time) VALUES
-	(1, 1, NULL, 'I am tired of these peaches. Maybe I try some with mozzarella.', '2025-02-22 18:52:48.300' ),
-	(1, 2, 1, 'Are you okay?' , '2025-02-24 18:52:48.300'),
+	(1, 1, NULL, 'I am tired of these peaches. Maybe I try some with mozzarella.', '2025-02-11 18:52:48.300' ),
+	(1, 2, 1, 'Are you okay?' , '2025-02-12 18:52:48.300'),
 
-	(2, 2, NULL, 'I am looking for a real bout burger in Southtown. Any recommendations?' , '2025-02-23 18:52:48.300'),
-	(2, 5, 3, 'Pigging out on fast food again?' , '2025-02-24 18:52:48.300'),
+	(2, 2, NULL, 'I am looking for a real bout burger in Southtown. Any recommendations?' , '2025-02-13 18:52:48.300'),
+	(2, 5, 3, 'Pigging out on fast food again?' , '2025-02-14 18:52:48.300'),
 
-	(3,3,NULL,'This girl with a brown ponytails been missing, anyone see her?' , '2025-02-27 18:52:48.300'),
-	(3,4,5,'I think I remember seeing someone like that near the train' , '2025-02-28 18:52:48.300'),
+	(3,3,NULL,'This girl with a brown ponytails been missing, anyone see her?' , '2025-02-15 18:52:48.300'),
+	(3,4,5,'I think I remember seeing someone like that near the train' , '2025-02-16 18:52:48.300'),
 
-	(4, 4, NULL, 'I cant hit these delays in this combo!' , '2025-02-24 18:52:48.300'),
-	(4, 2, 7, 'Try to match the rhythm with the ONE TWO THREE' , '2025-02-25 18:52:48.300'),
+	(4, 4, NULL, 'I cant hit these delays in this combo!' , '2025-02-17 18:52:48.300'),
+	(4, 2, 7, 'Try to match the rhythm with the ONE TWO THREE' , '2025-02-18 18:52:48.300'),
 
-	(5, 5, NULL, 'I havent been able to find him since yesterday' , '2025-02-21 18:52:48.300'),
-	(5, 2, 9, 'Hey Rock, im just getting busy around Southtown! Come on!' , '2025-02-22 18:52:48.300')
+	(5, 5, NULL, 'I havent been able to find him since yesterday' , '2025-02-19 18:52:48.300'),
+	(5, 2, 9, 'Hey Rock, im just getting busy around Southtown! Come on!' , '2025-02-20 18:52:48.300')
